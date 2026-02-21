eb31c8974df68053da5fbb8eeb99350658ae1773
Alejandra Guadalupe L. Altamirano <pandita420@MacBook-Pro.local>
2026-02-21 16:34:48 -0500

    agregamos api

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,7 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::get('/tours', [\App\Http\Controllers\Api\TourController::class, 'index']);
     Route::get('/tours/{slug}', [\App\Http\Controllers\Api\TourController::class, 'show']);
     Route::get('/tours/{slug}/availability', [\App\Http\Controllers\Api\TourController::class, 'availability']);
+    Route::get('/v1/me', fn (\Illuminate\Http\Request $r) => $r->user());
 });
+
+
