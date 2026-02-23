35d271c3adbcb3ed4abf6a3aa7d19ff7d7c395df
Alejandra Guadalupe L. Altamirano <pandita420@MacBook-Pro.local>
2026-02-23 17:29:48 -0500

    delete resource of frontend

--- resources/lang/en/home.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'destination_tagline' => 'Destination list',
-  'destination_title' => 'Explore Beautiful Places',
-  'destination_text' => 'Discover amazing experiences with carefully curated tours.',
-  'discover_more' => 'Discover more',
-];
--- resources/lang/es/home.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'destination_tagline' => 'Lista de destinos',
-  'destination_title' => 'Explora lugares hermosos',
-  'destination_text' => 'Descubre experiencias increíbles con tours cuidadosamente seleccionados.',
-  'discover_more' => 'Descubrir más',
-];
